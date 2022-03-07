142fddd82636a7f7a374c167f64ac6ea64298a2c
Tom Jia <tomchristian1998@gmail.com>
2022-03-07 15:40:54 +0800

    added the employee manager display when the app first runs

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const { askQuestions } = require("./helpers/switchLoop");
 //importing the loop that runs the code
 
+afterConnection = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+  };
+afterConnection()
 
 askQuestions();
